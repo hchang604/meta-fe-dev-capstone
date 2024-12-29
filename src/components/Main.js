44c168e23f98ff5ac7c0ddfda53b67520c4d22b6
hchang604 <hchang19948@gmail.com>
2024-12-29 14:31:37 -0800

    implement booking page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {useEffect, useReducer} from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
+import BookingPage from './Reservations/BookingPage';
+
+export const updateTimes = (state, action) => {
+
+  return {
+    ...state,
+    availableTimes: action.availableTimes
+  }
+}
+
+export const initializeTimes = () => {
+  return {
+    availableTimes: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
+  }
+}
 
 const Main = () => {
+    const [state, dispatch] = useReducer(updateTimes, initializeTimes())
+
+    function handleChangeAvailableTimes(newTimes) {
+      dispatch({availableTimes: newTimes})
+    }
+
     return <main>
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/about" element={<About />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/orderonline" element={<OrderOnline />} />
+        <Route path="/menu" element={<Menu />} /> */}
+        <Route path="/reservations" element={<BookingPage availableTimes={state.availableTimes} onAvailableTimesChange={handleChangeAvailableTimes} />} />
+        {/* <Route path="/orderonline" element={<OrderOnline />} />
         <Route path="/login" element={<Login />} /> */}
       </Routes>
     </main>
---
+++ src/components/Reservations/BookingPage.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { fetchAPI, submitAPI } from "../../API/api";
+
+const BookingPage = (props) => {
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+    const handleSubmit = () => {
+        setFormSubmitted(true)
+    }
+
+    return (
+        <>
+            <h1>Book Now</h1>
+            <BookingForm availableTimes={props.availableTimes} onAvailableTimesChange={props.onAvailableTimesChange} onSubmit={handleSubmit} />
+            {formSubmitted && <h2>Reservation confirmed</h2>}
+        </>
+    )
+}
+
+export default BookingPage
+
+const BookingForm = (props) => {
+    const [date, setDate] = useState('yyyy-mm-dd')
+    const [guestCount, setGuestCount] = useState(1)
+    const [occasion, setOccasion] = useState('Birthday')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.onSubmit()
+
+        if(submitAPI()) {
+            setDate('yyyy-mm-dd')
+            setGuestCount(1)
+            setOccasion('Birthday')
+        }
+    }
+    
+    useEffect(() => {
+        if(date === 'yyyy-mm-dd') {
+            props.onAvailableTimesChange([])
+            return
+        }
+
+        const getAvailableTimes = async () => {
+            const times = fetchAPI(new Date(date))
+            props.onAvailableTimesChange(times)
+        }
+  
+        getAvailableTimes()
+    }, [date])
+  
+
+    return (
+        <form style={{display: 'grid', maxWidth: '200px', gap: '20px'}} onSubmit={handleSubmit}>
+            <label for="res-date">Choose date</label>
+            <input aria-label="choose date" type="date" id="res-date" name="res-date" value={date} onChange={(e) => setDate(e.target.value)} />
+            <label for="res-time">Choose time</label>
+            <select aria-label="choose time" id="res-time" name="res-time">
+                {props.availableTimes.map((time) => <option key={time}>{time}</option>)}
+            </select>
+            <label for="guests">Number of guests</label>
+            <input aria-label="number of guests" type="number" placeholder="1" min="1" max="10" id="guests" name="guests" value={guestCount} onChange={(e) => setGuestCount(e.target.value)} />
+            <label for="occasion">Occasion</label>
+            <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+                <option>Birthday</option>
+                <option>Anniversary</option>
+            </select>
+            <input type="submit" value="Make Your reservation" />
+        </form>
+    )
+}
