c9a4790736ab302071d5a3a5ea8d06d6781e5592
hchang604 <hchang19948@gmail.com>
2024-12-30 14:59:43 -0800

    implement footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import logo from '../assets/Logo.svg'
 
 const Footer = () => {
-    return <footer></footer>
+    return (
+        <footer id="footer">
+            <img src={logo} />
+            <section id="contact">
+                <h3>Contact</h3>
+                <p>1111 ABC Street, Chicago, Illinois - 11111</p>
+
+                <p>111-111-1111</p>
+
+                <p>contact@littlelemon.com</p>
+            </section>
+        </footer>
+    )
 }
 
 export default Footer
